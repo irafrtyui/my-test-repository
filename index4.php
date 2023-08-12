68a1e7b4c1cad7e9bb5fd4ccaffc392c6a3b55ae
irafrtyui <irafrtyui@ukr.net>
2023-08-12 13:33:14 +0300

    Create README.md

--- index4.php
+++ index4.php
@@ -1,7 +1,7 @@
 <?php
 
-$p = [['image' => 'cards/water.jpg', 'name' => 'milk', 'price' => 40, 'weight' => 2000, 'vegan' => 'non-vegan', 'property' => ['dairy']],
-    ['image' => 'cards/' . $_FILES['image']['name'], 'name' => 'bread', 'price' => 15, 'weight' => 3000, 'vegan' => 'vegan', 'property' => ['cereals']],
-    ['image' => 'cards/' . $_FILES['image']['name'], 'name' => 'chocolate', 'price' => 35, 'weight' => 400, 'vegan' => 'non-vegan', 'property' => ['sweets']],
-    ['image' => 'cards/' . $_FILES['image']['name'], 'name' => 'chicken', 'price' => 50, 'weight' => 4000, 'vegan' => 'non-vegan', 'property' => ['meat']]];
+$p = [['image' => 'milk.jpg', 'name' => 'milk', 'price' => 40, 'weight' => 2000, 'vegan' => 'non-vegan', 'property' => ['dairy']],
+    ['image' => 'bread.jpg', 'name' => 'bread', 'price' => 15, 'weight' => 3000, 'vegan' => 'vegan', 'property' => ['cereals']],
+    ['image' => 'chocolate.jpg', 'name' => 'chocolate', 'price' => 35, 'weight' => 400, 'vegan' => 'non-vegan', 'property' => ['sweets']],
+    ['image' => 'chicken.jpg', 'name' => 'chicken', 'price' => 50, 'weight' => 4000, 'vegan' => 'non-vegan', 'property' => ['meat']]];
 file_put_contents('prod.txt',serialize($p));
